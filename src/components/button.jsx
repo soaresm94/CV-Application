d776a186384a62c2b6b9b591dd02ec8b7016e9a5
Guilherme <120758843+soaresm94@users.noreply.github.com>
2024-12-12 15:27:45 -0300

    Updates

--- src/components/button.jsx
+++
@@ -1,9 +0,0 @@
-export default function Button({text = "Submit", onClick}){
-    
-    const btnStyle = {
-        text: text
-    }
-    return(
-        <button style={btnStyle} onClick={onClick}>{text}</button>
-    )
-}
